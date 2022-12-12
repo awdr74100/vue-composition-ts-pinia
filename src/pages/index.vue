b4656c848d624f1df8212d0608c82604a8b54a25
Yi-Ru Lan <a78945612385238@gmail.com>
2022-12-13 02:42:34 +0800

    Update...

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <h1>Hello World</h1>
+  <h1>{{ greeting }}</h1>
+  <form
+    action="#"
+    @submit.prevent="user.signIn(email, password)"
+    v-if="!user.isSignIn"
+  >
+    <label for="email">信箱：</label>
+    <input type="email" id="email" v-model="email" />
+    <label for="password">密碼：</label>
+    <input type="password" id="password" v-model="password" />
+    <button type="submit">登入</button>
+  </form>
+  <button type="button" @click.prevent="user.signOut" v-else>登出</button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useUserStore } from '@/stores/user';
 
-<style scoped></style>
+const user = useUserStore();
+const { greeting } = storeToRefs(user);
+
+const email = ref('');
+const password = ref('');
+</script>
+
+<style></style>
---
+++ src/stores/user.ts
@@ -0,0 +1,41 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+
+// export const useUserStore = defineStore('user', {
+//   state: (): {
+//     isSignIn: boolean;
+//   } => ({
+//     isSignIn: false,
+//   }),
+//   getters: {
+//     greeting(state) {
+//       return state.isSignIn ? '用戶已登入' : '用戶尚未登入';
+//     },
+//   },
+//   actions: {
+//     signIn(email: string, password: string) {
+//       this.isSignIn = true;
+//     },
+//     signOut() {
+//       this.isSignIn = false;
+//     },
+//   },
+// });
+
+export const useUserStore = defineStore('user', () => {
+  const isSignIn = ref(false);
+
+  const greeting = computed(() => {
+    return isSignIn.value ? '用戶已登入' : '用戶尚未登入';
+  });
+
+  const signIn = (email: string, password: string) => {
+    isSignIn.value = true;
+  };
+
+  const signOut = () => {
+    isSignIn.value = false;
+  };
+
+  return { isSignIn, greeting, signIn, signOut };
+});
